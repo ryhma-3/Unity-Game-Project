59e73134ea2ab875e0b6ef3a4b3f61d52d77e067
wilbuuuur <ville.aalto@myy.haaga-helia.fi>
2023-02-14 12:08:24 +0200

    new mainmenu script

--- Unity-Game-Project/Assets/MainMenu.cs
+++ Unity-Game-Project/Assets/MainMenu.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Playgame ()
+    public void Playgame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame ()
+    public void QuitGame()
     {
         Debug.Log("QUIT");
         Application.Quit();
