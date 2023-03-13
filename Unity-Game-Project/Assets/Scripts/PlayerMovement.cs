537bbb7bbc68a8102c6e6507a665e31a7479e12e
AlexFrCava <112496181+AlexFrCava@users.noreply.github.com>
2023-03-13 17:35:51 +0200

    Comments

--- Unity-Game-Project/Assets/Scripts/PlayerMovement.cs
+++ Unity-Game-Project/Assets/Scripts/PlayerMovement.cs
@@ -2,27 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Hahmon ohjausfunktio
 public class PlayerMovement : MonoBehaviour
 {
-
+    // pelihahmon nopeuden säätäminen
     public float moveSpeed = 5f;
-
+    
+    //Rigidbody 2D määrittelee hahmon fysiikan.
     public Rigidbody2D rb;
+    // Animator määrittelee hahmon animaatiot.
     public Animator animator;
 
+    // Vector 2 määrittelee liikkumisen suunnan.
     Vector2 movement;
-
-
+    
+    // Update-funktio välittää näppäinkomennot pelille 
     void Update()
     {
+        //Vaakasuoraisen ja pystysuoraisen liikkumisen määrittely
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
+        
+        // Välittää arvoja pelaajan suunnasta animaattorille.
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
+        // Välittää arvon pelaajan nopeudesta animaattorille. 
         animator.SetFloat("Speed", movement.sqrMagnitude);
     }
-
+    // FixedUpdate-funktio liikuttaa pelaajaa. 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
