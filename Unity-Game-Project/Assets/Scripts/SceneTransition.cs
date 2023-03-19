6073b187cc02ed0307d47b97554a5f52bb7bec1e
Jesse Reunamo <112503770+jesreu@users.noreply.github.com>
2023-03-19 12:00:17 +0200

    Added a Effect on scene transitions

--- Unity-Game-Project/Assets/Scripts/SceneTransition.cs
+++ Unity-Game-Project/Assets/Scripts/SceneTransition.cs
@@ -8,13 +8,29 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue playerStorage;
+    public Animator transition;
+    public float transitionTime = 1f;
 
    public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player") && !other.isTrigger)
         {
             playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            LoadNextScene();
         }
+    }
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
