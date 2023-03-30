8e8c41c878ae5060f9427fbe4257db9fa1a671a4
divrrv <112497215+divrrv@users.noreply.github.com>
2023-03-30 14:22:11 +0300

    enemy scripts

---
+++ Unity-Game-Project/Assets/Scripts/skeleton.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skeleton : Enemy
+{
+    private Rigidbody2D myRigidbody;
+    public Transform target;
+    public float chaseRadius;
+    public float attacRadius;
+    public Transform homePosition;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = EnemyState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckDistance();
+        
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position,
+                            transform.position) <= chaseRadius
+            &&  Vector3.Distance(target.position,
+                                 transform.position) > attacRadius)
+        {
+            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
+            myRigidbody.MovePosition(temp);
+        }
+    }
+
+    private void ChangeState(EnemyState newState)
+    {
+        if(currentState != newState)
+        {
+            currentState = newState;
+        }
+    }
+}
