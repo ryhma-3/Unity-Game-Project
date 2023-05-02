931b948610db402eeaf17af1cc113e42401716bd
Jesse Reunamo <112503770+jesreu@users.noreply.github.com>
2023-05-02 09:50:44 +0300

    Added a sound to the death screen

--- Unity-Game-Project/Assets/Scripts/DeathScreen.cs
+++ Unity-Game-Project/Assets/Scripts/DeathScreen.cs
@@ -7,10 +7,13 @@ public class DeathScreen : MonoBehaviour
 
     public GameObject deathscreenUI;
     public static bool playerDead = false;
+
+    [SerializeField] private AudioSource deathSound;
     public void ActivateDeathScreen()
     {
         playerDead = true;
         deathscreenUI.SetActive(true);
+        deathSound.Play();
         Time.timeScale = 0f;
     }
 }
